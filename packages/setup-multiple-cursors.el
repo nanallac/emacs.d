c22cf04d82273581e49d22c19ce0ae7156ae1c51
Josh Callanan <josh@callanan.contact>
2026-08-27 12:42:05 +0800

    small update

--- packages/setup-multiple-cursors.el
+++ packages/setup-multiple-cursors.el
@@ -1,5 +1,5 @@
 (use-package multiple-cursors
   :bind (("C-S-c C-S-c" . mc/edit-lines)
-         ("C->" . mc/-next-like-this)
+         ("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
          ("C-c C-<" . mc/mark-all-like-this)))
