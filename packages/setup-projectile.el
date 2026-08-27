cc006e81008a117ae7a3f222854fc58db4f30cf0
Josh Callanan <josh@callanan.contact>
2026-08-27 20:47:56 +0800

    remove projectile keymap binding

--- packages/setup-projectile.el
+++ packages/setup-projectile.el
@@ -1,11 +1,8 @@
 (use-package projectile
-  :bind-keymap
-  (("s-p" . projectile-command-map))
 
   :bind
   ("C-x p p" . projectile-switch-project)
   ("C-x C-k" . projectile-kill-buffers)
 
   :config
-  (projectile-mode +1)
-  (define-key projectile-command-map (kbd "s-p") #'projectile-switch-project))
+  (projectile-mode +1))
